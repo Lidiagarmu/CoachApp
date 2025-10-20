b4e32da39a499e04a3b642d8edcc7998eae30b2a
Lidiagarmu <lidiagarmu@gmail.com>
2025-10-20 18:30:46 +0200

    colores principales extraidos al archivo tailwind.config.js

--- angular-frontend/tailwind.config.js
+++ angular-frontend/tailwind.config.js
@@ -1,19 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}", // detecta los ficheros donde Tailwind debe buscar clases
+    "./src/**/*.{html,ts}", // busca clases tailwind en tus componentes Angular
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#1D4ED8',    // Azul personalizado
-        secondary: '#9333EA',  // Morado personalizado
-        accent: '#F59E0B',     // Amarillo acento
+        primary: '#78ffd6',     // color principal (botones, detalles)
+        secondary: '#24243e',   // color secundario
+        hover: '#a8ff78',       // hover principal
+        darkbg: '#0f0c29',      // fondo oscuro de tarjetas, modales, etc.
       },
       fontFamily: {
-        sans: ['Poppins', 'sans-serif'], // Fuente predeterminada
+        sans: ['Poppins', 'sans-serif'], // fuente global
+      },
+      backgroundImage: {
+        'app-gradient': 'linear-gradient(to right, #78ffd6, #a8ff78)', // fondo global reutilizable
       },
     },
   },
   plugins: [],
-}
+};
