d348078f41f080004ebc6e3a15100a81cf3cc8f3
Lidiagarmu <lidiagarmu@gmail.com>
2025-11-03 13:00:48 +0100

    update team entity

--- symfony-backend/src/Entity/Team.php
+++ symfony-backend/src/Entity/Team.php
@@ -26,6 +26,14 @@ class Team
     #[ORM\Column(type: 'datetime_immutable')]
     private ?\DateTimeImmutable $createdAt = null;
 
+     #[ORM\PrePersist]
+    public function setCreatedAtValue(): void
+    {
+        if ($this->createdAt === null) {
+            $this->createdAt = new \DateTimeImmutable();
+        }
+    }
+
     public function __construct()
     {
         $this->players = new ArrayCollection();
