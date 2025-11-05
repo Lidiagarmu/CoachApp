df99fb81a2fac8eb265906f9279a74baf980d3d0
Lidiagarmu <lidiagarmu@gmail.com>
2025-11-05 16:55:09 +0100

    adaptando componentes coach y player dashboards para listaar jugadores

--- angular-frontend/src/app/modules/coach-dashboard/coach-dashboard.component.ts
+++ angular-frontend/src/app/modules/coach-dashboard/coach-dashboard.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { PlayerService, Player } from '../../services/player.service';
+import { TeamInvitationService } from '../../services/team-invitation.service';
 
 @Component({
   selector: 'app-coach-dashboard',
@@ -8,4 +10,32 @@ import { NavbarComponent } from '../../shared/components/navbar/navbar.component
   imports: [CommonModule, NavbarComponent],
   templateUrl: './coach-dashboard.component.html',
 })
-export class CoachDashboardComponent {}
+export class CoachDashboardComponent implements OnInit {
+  players: Player[] = [];
+
+  constructor(
+    private playerService: PlayerService,
+    private invitationService: TeamInvitationService
+  ) {}
+
+  ngOnInit() {
+    this.loadPlayers();
+  }
+
+  loadPlayers() {
+    this.playerService.getAvailablePlayers().subscribe({
+      next: (res) => (this.players = res),
+      error: (err) => console.error(err),
+    });
+  }
+
+  invitePlayer(playerId: number) {
+    this.invitationService.createInvitation(playerId).subscribe({
+      next: () => {
+        alert('Invitación enviada correctamente');
+        this.loadPlayers(); // refresca lista
+      },
+      error: (err) => alert(err.error?.error || 'Error al enviar invitación'),
+    });
+  }
+}
