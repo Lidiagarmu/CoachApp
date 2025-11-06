60e73d5162e58dc29a6ddac40a213f9b7f260e53
Lidiagarmu <lidiagarmu@gmail.com>
2025-11-06 14:05:09 +0100

    change endpoint in coach register teams to team

--- angular-frontend/src/app/modules/register/coach-register/coach-register.component.ts
+++ angular-frontend/src/app/modules/register/coach-register/coach-register.component.ts
@@ -55,7 +55,7 @@ export class CoachRegisterComponent implements OnInit {
 
   ngOnInit(): void {
     //  Cargar equipos
-    this.http.get<any[]>(`${environment.apiUrl}/teams`).subscribe({
+    this.http.get<any[]>(`${environment.apiUrl}/team`).subscribe({
       next: (res) => (this.teams = res),
       error: (err) => console.error('Error cargando equipos', err)
     });
