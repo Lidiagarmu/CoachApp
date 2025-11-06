a05c1c675618db66c9467dc16a714c9f03f23f82
Lidiagarmu <lidiagarmu@gmail.com>
2025-11-06 13:51:13 +0100

    add team controller and repository

---
+++ symfony-backend/migrations/Version20251106124343.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251106124343 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE team ADD shield VARCHAR(255) DEFAULT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE UNIQUE INDEX UNIQ_C4E0A61F5E237E06 ON team (name)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP INDEX UNIQ_C4E0A61F5E237E06
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE team DROP shield
+        SQL);
+    }
+}
